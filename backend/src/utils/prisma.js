19791e66979a11719f7c8c705b9fd7ba6b10f183
SamiSahil <samisahil498@gmail.com>
2025-11-08 17:46:37 +0600

    u

--- backend/src/utils/prisma.js
+++ backend/src/utils/prisma.js
@@ -1,6 +1,20 @@
-// src/utils/prisma.js
-const { PrismaClient } = require('@prisma/client');
+// backend/src/utils/prisma.js
 
-const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client');
+const { withAccelerate } = require('@prisma/client/extension-accelerate');
+
+// Use a single instance of Prisma Client
+let prisma;
+
+if (process.env.NODE_ENV === 'production') {
+  // In production (on Vercel), use the Accelerate-enabled Prisma Client
+  prisma = new PrismaClient().$extends(withAccelerate());
+} else {
+  // In development, use a global instance to avoid creating too many connections
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
 module.exports = prisma;
