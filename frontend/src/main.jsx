db348c3adf34f751e431d301104fcdd279216d00
SamiSahil <samisahil498@gmail.com>
2025-11-08 19:23:14 +0600

    d

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' // BrowserRouter এর পরিবর্তে HashRouter
-import App from './App.jsx'
-import { ThemeProvider } from './contexts/ThemeContext.jsx'
-import { AuthProvider } from './contexts/AuthContext.jsx'
-import './styles/globals.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <-- Change this
+import App from './App.jsx';
+import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import { AuthProvider } from './contexts/AuthContext.jsx';
+import './styles/globals.css'; // Assuming you have this file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter> {/* BrowserRouter এর পরিবর্তে HashRouter */}
+    <BrowserRouter> {/* <-- And change this */}
       <ThemeProvider>
         <AuthProvider>
           <App />
         </AuthProvider>
       </ThemeProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
